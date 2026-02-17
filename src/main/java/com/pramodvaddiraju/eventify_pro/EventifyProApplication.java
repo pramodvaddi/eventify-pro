41db9fedfd0f9033f475b95ab906fc3d2b279801
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-17 13:11:32 +0530

    initial commit

---
+++ src/main/java/com/pramodvaddiraju/eventify_pro/EventifyProApplication.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.eventify_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifyProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventifyProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramodvaddiraju/eventify_pro/EventifyProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.eventify_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
